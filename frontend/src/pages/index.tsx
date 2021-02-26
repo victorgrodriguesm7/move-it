fad4bebe39bc1d7edbd88b3bad01394b97739d7d
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-26 18:18:56 -0300

    Home created

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import ChallengeBox from '../components/ChallengeBox';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import Profile from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar currentExp={300} maxExp={600}/>
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+          <Countdown/>
+        </div>
+        <div>
+          <ChallengeBox/>
+        </div>
+      </section>
+    </div>
+  );
+}
