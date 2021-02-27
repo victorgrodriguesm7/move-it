179bdcad76ebdd3930b34735c4d28579d8b88365
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-27 14:40:47 -0300

    Added ChallengesContext integration

--- frontend/src/components/ExperienceBar.tsx
+++ frontend/src/components/ExperienceBar.tsx
@@ -1,22 +1,22 @@
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
-interface ExperienceBarProps {
-    currentExp: number;
-    maxExp: number;
-}
 
-export function ExperienceBar({currentExp, maxExp}: ExperienceBarProps){
-    let { currentExp, maxExp } = props;
-    let percentage = (currentExp * 100) / maxExp;
+export function ExperienceBar(){
+    let { currentExperience, experienceToNextLevel } = useContext(challengesContext);
+    let percentageToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
     
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{width: `${percentage}%`}}></div>
-                <span className={styles.currentExperience} style={{left: `${percentage}%`}}>{currentExp} exp</span>
+                <div style={{width: `${percentageToNextLevel}%`}}></div>
+                <span className={styles.currentExperience} style={{left: `${percentageToNextLevel}%`}}>
+                    {currentExperience} exp
+                </span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     );
 }
